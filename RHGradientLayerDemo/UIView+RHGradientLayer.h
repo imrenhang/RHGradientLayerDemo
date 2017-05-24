92541f938a07e15e467cb4ba5b1ada2a1b078399
imrenhang <renhang@renhangdeMacBook-Pro.local>
2017-05-24 09:34:38 +0800

    RHGradientLayer
    RHGradientLayer

---
+++ RHGradientLayerDemo/UIView+RHGradientLayer.h
@@ -0,0 +1,21 @@
+//
+//  UIView+RHGradientLayer.h
+//  CATextLayer
+//
+//  Created by 任航 on 2017/5/23.
+//  Copyright © 2017年 任航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Utils)
+
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, strong) UIColor *textColor;
+@property (nonatomic, strong) UIFont *font;
+
+@property (nonatomic, strong) NSString *fromColor;
+@property (nonatomic, strong) NSString *toColor;
+
+//+ (CAGradientLayer *)setGradualChangingColor:(UIView *)view fromColor:(NSString *)fromHexColorStr toColor:(NSString *)toHexColorStr;
+@end
--- RHGradientLayerDemo/ViewController.m
+++ RHGradientLayerDemo/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "UIView+RHGradientLayer.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //用UIView实现
+    UIView *testView = [UIView new];
+    
+    testView.frame = CGRectMake(60, 60, 110, 40);
+    
+    testView.layer.cornerRadius = 20.0;
+    
+    testView.layer.masksToBounds = YES;
+    
+    testView.font = [UIFont systemFontOfSize:14];
+    
+    /************************************
+     **   tip:注意fromColor和toColor的顺序
+     **   fromColor在前, toColor在后
+     **   fromCOlor toColor为渐变色的区间
+     ************************************/
+    
+    testView.fromColor = @"F76B1C";
+    
+    testView.toColor = @"FBDA61";
+    
+    testView.text = @"可爱的UIView";
+    
+    //如要设置字体颜色
+    //testView.textColor = [UIColor redColor];
+    
+    [self.view addSubview:testView];
+
 }
 
 
